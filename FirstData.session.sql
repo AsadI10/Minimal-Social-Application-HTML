266ec301698a10721a0ddd44dfda847838faa9f6
31005646 <31005646@my.shu.ac.uk>
2022-03-03 10:21:36 +0000

    first commit

---
+++ FirstData.session.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE accounts(
+    ID INT(5),
+    username VARCHAR(50),
+    emailaddress VARCHAR(50),
+    passwords VARCHAR(16),
+    PRIMARY KEY (ID)
+)
---
+++ JS/main.js
@@ -0,0 +1,34 @@
+function User(){
+    location.href = "registration.html";
+}
+function Regist(){
+    location.href = "registertext.html"
+    // {if (Regist("click ok to continue")) window.location = "registertext.html";}
+}
+function UserLog(){
+    location.href = "userlogin.html";
+}
+function Adminlog(){
+    location.href = "adminlogin.html";
+}
+function Let(){
+    location.href = "userlogin.html";
+}
+function Userclick(){
+    location.href = "profile.html";
+}
+function LogOff(){
+    location.href = "index.html";
+}
+function Login(){
+    location.replace("userhomepage.html");
+}
+function AdminInfo(){
+    location.href = "adminhomepage.html";
+}
+function AdminLogoff(){
+    location.href = "index.html";
+}
+function Approvepo(){
+    location.href = "approvepost.html";
+}
